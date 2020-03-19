9ee1d9509207616ef13b6cdf120969a25f46eb7c
yhzddy <1194725376@qq.com>
2020-03-19 17:58:21 +0800

    修改字体问题

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,20 @@
+// var utils = require ('./utils.js')
+
 module.exports={
     publicPath: "./",
     assetsDir: "libs",
-    configureWebpack: (config) => {
-        config.module.rules.push({
-          // 处理字体
-          test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-          use: [{
-            loader: 'url-loader',
-            options: {
-                limit: 10000,
-                name: 'resourse/[name].[hash:7].[ext]'
-            }
-          }]
-        })
-      }
+    // configureWebpack: (config) => {
+    //     config.module.rules.push({
+    //       // 处理字体
+    //       test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+    //       use: [{
+    //         loader: 'url-loader',
+    //         options: {
+    //             limit: 10000,
+    //             // name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+    //             name: 'resourse/[name].[hash:7].[ext]'
+    //         }
+    //       }]
+    //     })
+    //   }
 }
